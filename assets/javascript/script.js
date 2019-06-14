6bae480d18fc71a20911b80687dd21e6ba075303
jfrancoc <jfcslc@outlook.com>
2019-06-13 21:43:49 -0600

    update variables: select document, set property

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,10 +8,15 @@ function myFunction() {
 const inputs = document.querySelectorAll('.controls input');
 
 // update handler
-function handleUpdate(){
-    // console.log('UPDATE :', this.value);
-    // def suffix as constant with value of sizing
-    const suffix = this.dataset.sizing;
+function handleUpdate() {
+    // console log this.value
+    console.log('UPDATE :', this.value);
+    // def suffix as constant with value of sizing or nothing
+    const suffix = this.dataset.sizing || '';
+    // console suffix
+    console.log('SUFFIX :', suffix)
+    // update variables: select document, set property --base --spacing --blur
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 // inputs on change event listener
