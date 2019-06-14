f985a228d4f2477ba6adc346dc7348b8fad4b3e1
jfrancoc <jfcslc@outlook.com>
2019-06-13 21:49:45 -0600

    updates to line notes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,3 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "SUCCESS!";
-    var x = document.getElementsByTagName("BODY")[0];
-    x.style.backgroundColor = "red";
-}
-
 // define inputs as a constant and query .controls input
 const inputs = document.querySelectorAll('.controls input');
 
@@ -14,7 +8,7 @@ function handleUpdate() {
     // def suffix as constant with value of sizing or nothing
     const suffix = this.dataset.sizing || '';
     // console suffix
-    console.log('SUFFIX :', suffix)
+    console.log('SUFFIX :', this.dataset)
     // update variables: select document, set property --base --spacing --blur
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
