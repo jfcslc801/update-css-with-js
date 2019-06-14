5bac6cf9297161c9507eb038a1d279e4e86c0da8
jfrancoc <jfcslc@outlook.com>
2019-06-13 21:07:28 -0600

    def inputs as a constant and query .controls input

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,3 +3,12 @@ function myFunction() {
     var x = document.getElementsByTagName("BODY")[0];
     x.style.backgroundColor = "red";
 }
+
+// define inputs as a constant and query .controls input
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate(){
+    console.log(this.value);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
